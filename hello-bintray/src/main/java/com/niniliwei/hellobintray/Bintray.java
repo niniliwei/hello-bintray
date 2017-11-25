e50dfcece2489b406fd28b353522d7c676e6b802
liwei <liwei@uama.com.cn>
2017-11-25 19:29:43 +0800

    initial commit

---
+++ hello-bintray/src/main/java/com/niniliwei/hellobintray/Bintray.java
@@ -0,0 +1,11 @@
+package com.niniliwei.hellobintray;
+
+/**
+ * Created by well on 2017/11/25.
+ */
+
+public class Bintray {
+    public static void hello() {
+        System.out.println("Hello Bintray!");
+    }
+}
